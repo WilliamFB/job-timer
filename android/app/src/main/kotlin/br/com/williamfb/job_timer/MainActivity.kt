fd906ec8fbc197574cf28cc43f98767a1e757510
William <williambarreiro1@hotmail.com>
2022-06-07 20:48:37 -0300

    Implementação do login

---
+++ android/app/src/main/kotlin/br/com/williamfb/job_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.williamfb.job_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_module.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'package:job_timer/app/services/auth_service_impl.dart';
+
+import 'modules/home/home_module.dart';
+import 'services/auth_service.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds =>
+      [Bind.lazySingleton<AuthService>((i) => AuthServiceImpl())];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const SplashPage()),
+        ModuleRoute('/login', module: LoginModule()),
+        ModuleRoute('/home', module: HomeModule()),
+      ];
+}
